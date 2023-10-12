a6a158db41f32e818da9d29fbc69c1b5445092bc
alexshumteru <alexshum939@gmail.com>
2023-10-12 22:06:41 +0800

    Fixed blur filter cancels modal position fixed

--- client/src/components/Background.tsx
+++ client/src/components/Background.tsx
@@ -1,21 +1,14 @@
+import './Background.css';
+
 type BackgroundProp = {
     children?: React.ReactNode; 
 }
 
 const Background = ({children}:BackgroundProp) => {
   return (
-    <div style={
-      {
-        background: "linear-gradient(135deg, rgba(0,224,255,1) 0%, rgba(255,183,116,1) 100%)"
-      }
-    }>
-      <div style={
-        {
-          backgroundImage: "radial-gradient(circle at 24px 24px, rgba(168, 234, 254, 0.5) 12px, transparent 0)",
-          backgroundSize: "48px 48px"
-        }
-      }>
-          <div style={{backdropFilter: "blur(4px)"}}>
+    <div className='gradient-layer'>
+      <div className='dots-layer'>
+          <div className='blur-layer'>
             {children}
           </div>
       </div>
