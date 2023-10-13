c8d1fcb616bb120119775e214aa26b2e5c80f197
alexshumteru <alexshum939@gmail.com>
2023-10-14 02:44:59 +0800

    Added logic for a player to create room in RoomList.ts and GameRoom.ts and rewrote the method for join room with id in RoomList.ts, and added the initial version of their socket handlers

--- server/src/custom-classes/GameRoom.ts
+++ server/src/custom-classes/GameRoom.ts
@@ -1,28 +1,46 @@
 import {Player} from "./Player.js";
 
+
+//TODO create a new method that can randomly pick someone to be be the WORD_PICKER.
+//obviously keeping in mind who has already been a WORD_PICKER for that round
+//that way if the current word WORD_PICKER is removed another can be easily picked
+
+//TODO create a new method that will randomly pick a new host, if a host disconnets
+
+//TODO create a new method that will only allow a game to progress if the game only
+//3 or more players.
+
 export class GameRoom {
   id: string;
   players: Player[];
+  host: Player;
   targetWord: string;
+  roomMaxCapacity: number;
 
-  constructor(id: string, players: Player[]) {
+  constructor(id: string, players: Player[], roomMaxCapcity?: number) {
     this.id = id;
     this.players = players;
+    this.roomMaxCapacity = typeof roomMaxCapcity === "undefined"? 5 : roomMaxCapcity;
+    if (players.length > 0) {
+      this.host = players[0];
+    }
   }
 
-  addPlayer(player: Player) {
+  addPlayer(player: Player): boolean {
+    if (this.players.length >= this.roomMaxCapacity) {
+      return false;
+    }
     if (this.players.length === 0) {
       player.role = "WORD_PICKER";
+      this.host = player;
     }
     player.roomId = this.id;
     this.players.push(player);
+    return true;
   }
 
   removePlayer(playerId: string): GameRoom | undefined {
     let found: boolean = false;
-    //TODO create a new method that can randomly pick someone to be be the WORD_PICKER.
-    //obviously keeping in mind who has already been a WORD_PICKER for that round
-    //that way if the current word WORD_PICKER is removed another can be easily picked
 
     this.players = this.players.filter((player) => {
       if (playerId === player.id) {
@@ -37,5 +55,13 @@ export class GameRoom {
     } else {
       return;
     }
+  }
+
+  setRoomMaxCapacity(newCapacity: number) {
+    if (newCapacity < 3 || newCapacity > 5) return;
+
+    if (this.players.length > newCapacity) return;
+
+    this.roomMaxCapacity = newCapacity;
   }
 }
--- server/src/sockets/socketEventHandlers.ts
+++ server/src/sockets/socketEventHandlers.ts
@@ -3,18 +3,21 @@ import { io } from "../index.js";
 import {RoomList} from "../custom-classes/RoomList.js";
 import {Player} from "../custom-classes/Player.js";
 
-type RoomJoinData = {
+//TODO add handlers for removing a player
+
+type RoomChangeData = {
   userName: string;
+  roomId?: string; 
 };
 
-export const handleNewMessageEvent = (data, socket: Socket) => {
+export const handleNewMessageEvent = (data: RoomChangeData, socket: Socket) => {
   io.emit("recieve_message", data);
 };
 
-export const handleRoomJoin = (data: RoomJoinData, socket: Socket) => {
+export const handleRoomJoin = (data: RoomChangeData, socket: Socket) => {
   const newPlayer = new Player(socket.id, data.userName);
 
-  const freeRoom = RoomList.addPlayerToRoom(newPlayer);
+  const freeRoom = RoomList.addPlayerToFirstAvailableRoom(newPlayer);
 
   socket.join(freeRoom.id);
   io.to(freeRoom.id).emit("room_message", {
@@ -25,3 +28,28 @@ export const handleRoomJoin = (data: RoomJoinData, socket: Socket) => {
 
   console.log("Rooms: ", RoomList.rooms);
 };
+
+export const handleRoomJoinWithID = (data: RoomChangeData, socket: Socket) => {
+  const newPlayer = new Player(socket.id, data.userName);
+
+  const room = RoomList.addPlayerToRoomWithId(data.roomId, newPlayer);
+
+  if (room) {
+    socket.join(room.id);
+    io.to(room.id).emit("room_message", {
+      type: "ROOM_UPDATE",
+      message: `User: ${newPlayer.userName} has joined ${room.id}`,
+      roomInfo: RoomList.rooms[room.id]
+    });
+  } else {
+    console.log("No such roomes exists");
+  }
+}
+
+export const handleCreateRoom = (data: RoomChangeData, socket: Socket) => {
+  const newPlayer = new Player(socket.id, data.userName);
+
+  const newRoom = RoomList.createRoom(newPlayer);
+  socket.join(newRoom.id);
+
+}
