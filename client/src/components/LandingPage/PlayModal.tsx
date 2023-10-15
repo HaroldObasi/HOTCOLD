4abbc043f58359167587782a42bc88fb22b75056
Prasad Shinde <sketchware1@gmail.com>
2023-10-15 22:15:47 +0530

    added find rooms modal

--- client/src/components/LandingPage/PlayModal.tsx
+++ client/src/components/LandingPage/PlayModal.tsx
@@ -1,37 +1,46 @@
-import React, { useEffect, useRef } from 'react'
+import React, {useEffect, useRef} from "react";
 
 type PlayModalProps = {
   open: boolean;
   onClose: () => void;
-  children?: React.ReactNode;    
-}
+  children?: React.ReactNode;
+};
 
 const PlayModal = ({open, onClose, children}: PlayModalProps) => {
-
   useEffect(() => {
     const onKeyPress = (e: KeyboardEvent) => {
       if (open && e.key === "Escape") onClose();
     };
-    
+
     window.addEventListener("keydown", onKeyPress);
-      return () => window.removeEventListener("keydown", onKeyPress);
+    return () => window.removeEventListener("keydown", onKeyPress);
   }, [onClose, open]);
-    
+
   const container = useRef<HTMLDivElement>(null);
   const onOverlayClick = (e: React.MouseEvent) => {
-    if (!container.current?.contains(e.target as Node)) {         
+    console.log(
+      "onOverlayClick",
+      container.current?.contains(e.target as Node)
+    );
+    if (!container.current?.contains(e.target as Node)) {
       onClose();
     }
   };
- 
 
   return (
     <div
-      className={`fixed inset-0 z-10 p-4 bg-gray-900/50 ${open ? "block" : "hidden"}`} 
+      className={`fixed inset-0 z-10 p-4 bg-gray-900/50 ${
+        open ? "block" : "hidden"
+      }`}
       onClick={onOverlayClick}
     >
-      <div className="relative top-[10%] w-full max-w-[600px] mx-auto mt-8" ref={container}>
-        <div className="pt-8 pb-12 overflow-hidden bg-gray-200/70 rounded-3xl shadow-xl">{children}</div>
+      <div
+        className="relative top-[10%] w-full max-w-[600px] mx-auto mt-8"
+        ref={container}
+      >
+        <div className="py-4 overflow-hidden bg-gray-200/80 rounded-3xl shadow-xl">
+          {children}
+        </div>
       </div>
     </div>
   );
