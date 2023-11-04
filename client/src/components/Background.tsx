6cbbc9f52725d8b7bfb22529fe423d8679af2d54
HaroldObasi <haroldobasi2k16@gmail.com>
2023-11-04 22:04:09 +0100

    added additional prop for style change

--- client/src/components/Background.tsx
+++ client/src/components/Background.tsx
@@ -1,19 +1,18 @@
 import './Background.css';
 
 type BackgroundProp = {
-    children?: React.ReactNode; 
-}
+  children?: React.ReactNode;
+  className?: string;
+};
 
-const Background = ({children}:BackgroundProp) => {
+const Background = ({children, className}: BackgroundProp) => {
   return (
-    <div className='gradient-layer'>
-      <div className='dots-layer'>
-          <div className='blur-layer'>
-            {children}
-          </div>
+    <div className={`gradient-layer ${className}`}>
+      <div className="dots-layer h-full">
+        <div className="blur-layer">{children}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Background;
