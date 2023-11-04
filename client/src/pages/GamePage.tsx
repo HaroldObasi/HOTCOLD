2b5a160f6323ef55099d24365a14bf7966253980
HaroldObasi <haroldobasi2k16@gmail.com>
2023-11-04 23:27:31 +0100

    worked on messaging feature

--- client/src/pages/GamePage.tsx
+++ client/src/pages/GamePage.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
+import {socket} from "../socket";
 import {useSelector} from "react-redux";
 import {RootState} from "../state/PlayerStore";
 import Background from "../components/Background";
+import Message from "../components/GamePage/Message";
 
 type Props = {};
 
 const GamePage = (props: Props) => {
   const room = useSelector((state: RootState) => state.game.room);
+  const player = useSelector((state: RootState) => state.player);
+  const [guess, setGuess] = useState<string>("");
+
+  function handleSendMessage() {
+    const data = {
+      sender: player,
+      message: guess,
+      timeSent: Date.now(),
+      rating: null,
+      correct: false,
+      roomId: player.roomId
+    };
+    socket.emit("send_message", data);
+  }
 
   return (
     <div className="font-dela">
@@ -42,13 +58,33 @@ const GamePage = (props: Props) => {
 
           <div className="bg-slate-200 flex-1 relative">
             chat
-            <ul>
+            <ul className="font-denk">
               {room.messages.map((item: any) => {
-                return <li>{item.message}</li>;
+                return (
+                  <li>
+                    <Message
+                      senderName={item.sender.userName}
+                      rating={item.rating}
+                      message={item.message}
+                    />
+                  </li>
+                );
               })}
             </ul>
-            <div className="absolute bottom-0">
-              <input type="text" />
+            <div className="absolute bottom-0 w-full flex gap-x-1">
+              <input
+                className="font-denk font-light py-2 px-2 ring-0 outline-none w-full"
+                type="text"
+                onChange={(e) => {
+                  setGuess(e.target.value);
+                }}
+              />
+              <button
+                onClick={handleSendMessage}
+                className="px-3 bg-emerald-400 hover:bg-emerald-700 text-white"
+              >
+                Send
+              </button>
             </div>
           </div>
         </div>
