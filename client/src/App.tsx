89f272e011814dfd0510bc99d78f941c7315e224
HaroldObasi <haroldobasi2k16@gmail.com>
2023-11-04 22:03:54 +0100

    root app component listens to room_message

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,37 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {useEffect} from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import {RootState} from "./state/PlayerStore";
+import GamePage from "./pages/GamePage";
+import {useSelector, useDispatch} from "react-redux";
+import {changeGameState} from "./state/GameSlice";
+import {socket} from "./socket";
 
 function App() {
+  const room = useSelector((state: RootState) => state.game.room);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    function onRoomMessage(value: any) {
+      console.log("room message: ", value);
+      dispatch(changeGameState(value.roomInfo));
+    }
+
+    socket.on("room_message", onRoomMessage);
+
+    return () => {
+      socket.off("room_message");
+    };
+  }, [room]);
+
   return (
     <main>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage/>}>
-
-          </Route>
+          <Route
+            path="/"
+            element={room === null ? <LandingPage /> : <GamePage />}
+          ></Route>
         </Routes>
       </BrowserRouter>
     </main>
