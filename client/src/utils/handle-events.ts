5cf0cfdb1be2b4eff37118e6e6dff0513f8bc67c
Harold Obasi <haroldobasi2k16@gmail.com>
2023-12-01 21:56:35 +0100

    added handler for guess update

--- client/src/utils/handle-events.ts
+++ client/src/utils/handle-events.ts
@@ -7,7 +7,6 @@ const handleGameStarted = (dispatch: any, room: any) => {
 };
 
 const handleTimerTick = (dispatch: any, room: any) => {
-  console.log("ticker: ", room);
   dispatch(changeGameTimer(room.timer));
 };
 
@@ -21,6 +20,10 @@ const handleRoomUpdate = (dispatch: any, room: any) => {
   dispatch(changeGameState(room.roomInfo));
 };
 
+const handleGuessRated = (dispatch: any, room: any) => {
+  dispatch(changeGameState(room.roomInfo));
+};
+
 type Events = {
   [key: string]: (dispatch: any, room: any) => void;
 };
@@ -29,15 +32,6 @@ export const events: Events = {
   GAME_STARTED: handleGameStarted,
   GAME_TIMER_TICK: handleTimerTick,
   ROOM_UPDATE: handleRoomUpdate,
-  NEW_ROOM_MESSAGE: handleRoomMessage
-};
-
-const switchEvents = (message: {type: string; message: string}) => {
-  switch (message.type) {
-    case "GAME_STARTED":
-      break;
-
-    default:
-      break;
-  }
+  NEW_ROOM_MESSAGE: handleRoomMessage,
+  GUESS_RATING_UPDATE: handleGuessRated
 };
