329ccc93d1d0898d2509a58dac1b51611b5b6dfb
Harold Obasi <haroldobasi2k16@gmail.com>
2024-07-11 23:09:56 +0100

    complete modal

--- client/src/components/modals/SelectWordModal.tsx
+++ client/src/components/modals/SelectWordModal.tsx
@@ -1,14 +1,70 @@
 import {useSelector} from "react-redux/es/hooks/useSelector";
 import {RootState} from "../../state/PlayerStore";
+import axios from "axios";
+import {useState} from "react";
+
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle
+} from "../ui/alert-dialog";
+import {Button} from "../ui/button";
+import {useDispatch} from "react-redux";
+import {setModal} from "../../state/uiSlice";
 
 const SelectWordModal = () => {
-  const {modalOpen} = useSelector((state: RootState) => state.ui);
+  const apiUrl = import.meta.env.VITE_SOCKET_IO_URL || "http://localhost:5000";
 
-  if (modalOpen) {
-    return <div>SelectWordModal</div>;
-  }
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+  const {id: roomId} = useSelector((state: RootState) => state.game.room);
+  const {modalOpen, targetWordOptions} = useSelector(
+    (state: RootState) => state.ui
+  );
 
-  return <></>;
+  console.log("tw ", targetWordOptions);
+
+  const onSelectOption = async (index: number) => {
+    try {
+      setLoading(true);
+      await axios.post(`${apiUrl}/api/guess/selectTargetWord`, {
+        wordIndex: index,
+        roomId: roomId
+      });
+      dispatch(setModal(false));
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <AlertDialog open={modalOpen}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle className="text-center">
+            Your turn to pick a word!
+          </AlertDialogTitle>
+        </AlertDialogHeader>
+        <div className="flex justify-between">
+          {targetWordOptions.map((item, key) => (
+            <Button
+              disabled={loading}
+              className="bg-sky-300 active:bg-sky-500 hover:bg-sky-500 rounded-full"
+              key={key}
+              onClick={() => onSelectOption(key)}
+            >
+              {item}
+            </Button>
+          ))}
+        </div>
+        <AlertDialogFooter></AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 };
 
 export default SelectWordModal;
